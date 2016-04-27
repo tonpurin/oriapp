5871c08a4ded479ecfc44516de8420b170cbd09f
片岡一平 <tonpurin2510@gmail.com>
2016-04-27 11:47:52 +0900

    top#indexでuser_itemsからレコード取得 indexのviewで部分テンプレート使用

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,8 @@ class TopController < ApplicationController
 
   def index
     @items = Item.limit(20)
-    @user_item = UserItem.new
+    @user_items = UserItem.all
+    @new_user_item = UserItem.new
     # JSでも利用可能な変数
     # idでハッシュに変換
     gon.items = Item.to_hash(@items, "id")
