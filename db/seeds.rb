6182f338d900627bb68753a606a2a4eef764c391
片岡一平 <tonpurin2510@gmail.com>
2016-04-27 10:56:25 +0900

    seeds.rbを修正 モデル名の変更を適用

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@
 require "csv"
 
 CSV.foreach('db/zyaran_scrap_geocode.csv') do |row|
-  Shop.create(:shop_name => row[0], :rate => row[1], :shop_pref => row[2], :shop_genre => row[3], :image_url => row[4], :shop_zip => row[5], :shop_address => row[6], :shop_url => row[7], :shop_lat => row[8], :shop_lng => row[9])
+  Item.create(:item_name => row[0], :rate => row[1], :item_pref => row[2], :item_genre => row[3], :image_url => row[4], :item_zip => row[5], :item_address => row[6], :item_url => row[7], :item_lat => row[8], :item_lng => row[9])
 end
