4dbc96b49198c26af66ed66f3046ed4b04c73d26
片岡一平 <tonpurin2510@gmail.com>
2016-04-30 09:48:01 +0900

    ビューの調整  - ヘッダーの色変更  - 背景に画像使用  - ドロップダウンメニューの追加

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -27,10 +27,30 @@ create_map = function() {
     var mapOptions = {
       zoom: 15 ,        // ズーム値
       center: latlng ,    // 中心座標 [latlng]
+
+      // ---------- マップのスタイル設定 ---------
+      //mapTypeId: google.maps.MapTypeId.ROADMAP,  //マップタイプ
+      // mapTypeControlOptions: {
+      //   mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'simple']
+      // }
     };
 
     // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
     map = new google.maps.Map( canvas , mapOptions );
+
+    // 以下google mapのスタイル変更
+    // http://blog.prostaff1.com/google/1150/
+    // https://snazzymaps.com/style/70/unsaturated-browns
+    // var samplestyle = [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}];
+
+    // var samplestyleOptions = {
+    //   name: "シンプル"
+    // };
+
+    // var sampleMapType = new google.maps.StyledMapType(samplestyle, samplestyleOptions);
+    // map.mapTypes.set('simple', sampleMapType);
+    // map.setMapTypeId('simple');
+
   }else{
     console.log('there is a map already');
   };
@@ -104,4 +124,7 @@ init_gmap = function(){
   move_map_center(last_marker[0], last_marker[1]);
 
 };
+
+
+
 
