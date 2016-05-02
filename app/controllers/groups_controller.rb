e7b0252e3a140d6b7675a9eb4c4c475f7ad9185e
片岡一平 <tonpurin2510@gmail.com>
2016-05-02 10:25:10 +0900

    グループ作成のためのコントローラーとビューを作成 newアクションにルーティング

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,6 @@
+class GroupsController < ApplicationController
+
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "top#index"
   resources :top, only: [:index, :create, :destroy]
   get "top/index/:id" => 'top#index'
+  resources :groups, only: :new
   devise_for :users
 
 end
