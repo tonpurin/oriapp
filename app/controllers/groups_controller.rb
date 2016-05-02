8c1712f8d68ec423473b3e7d97776fda2781c781
片岡一平 <tonpurin2510@gmail.com>
2016-05-02 10:41:24 +0900

    form_forを用いてグループ作成画面を構築

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,10 @@
 class GroupsController < ApplicationController
 
   def new
+    @new_group = Group.new
+  end
+
+  def create
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "top#index"
   resources :top, only: [:index, :create, :destroy]
   get "top/index/:id" => 'top#index'
-  resources :groups, only: :new
+  resources :groups, only: [:new, :create]
   devise_for :users
 
 end
