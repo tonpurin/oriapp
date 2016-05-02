81bd9a1ec1892d5fe4b72c5fd7de28d8e32b9c05
片岡一平 <tonpurin2510@gmail.com>
2016-05-02 16:32:56 +0900

    userにユニークネームを保存可能に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # deviseにカラムを追加し，DBに保存する場合に必要な記述
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :unique_name
+  end
 end
