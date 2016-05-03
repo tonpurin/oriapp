c0850b86b4c9ee995afe04e20ce735227ee4d0ed
片岡一平 <tonpurin2510@gmail.com>
2016-05-03 13:10:01 +0900

    userのユニークネームでログイン可能に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
   # deviseにカラムを追加し，DBに保存する場合に必要な記述
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :unique_name
+      # sign_upにユーザネームを設定
+      devise_parameter_sanitizer.for(:sign_up) { |u|
+        u.permit(:email, :unique_name, :password, :password_confirmation)
+      }
+      devise_parameter_sanitizer.for(:sign_in) { |u|
+        u.permit(:unique_name, :password, :remember_me)
+    }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,32 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:unique_name]
 
-  # user_idを必須、一意とする
+  # email, unique_nameを必須、一意とする
   # 参照 : http://qiita.com/itboze/items/385d3cd539fc7578b52a
-  validates_uniqueness_of :unique_name
-  validates_presence_of :unique_name
+  validates_uniqueness_of [:email, :unique_name]
+  validates_presence_of [:email, :unique_name]
+
+   # unique_nameを仕様してログインするようオーバーライド
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      # 認証の条件式を変更する
+      where(conditions).where(["unique_name = :value", {:value => unique_name}]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  # 登録時にemailを不要とする
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 
   # paperclip用
   has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }
