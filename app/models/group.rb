f216782eb3193c3a8212e332d7f3aba8ef3a11ca
片岡一平 <tonpurin2510@gmail.com>
2016-05-06 11:22:08 +0900

    group作成時にデフォルトでno-image.pngがアバターに登録される

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,16 @@ class Group < ActiveRecord::Base
 
   has_many :user_groups
 
+  # 新規グループのレコードを作成する際にデフォルト値を設定
+  after_initialize :set_default, if: :new_record?
+
   # paperclip用
   has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+
+  private
+  def set_default
+    self.avatar = File.new("app/assets/images/no-image.png", "r")
+  end
 
 end
