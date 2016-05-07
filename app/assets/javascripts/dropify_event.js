e570c799616374c60931bb75f57050f4376ef82e
片岡一平 <tonpurin2510@gmail.com>
2016-05-07 17:59:17 +0900

    document.URL.matchの書き方を賢くした(笑)

--- app/assets/javascripts/dropify_event.js
+++ app/assets/javascripts/dropify_event.js
@@ -2,11 +2,7 @@ $( function() {
 
   // /users/edit or /groups/newに遷移したら実行される
   // 参考 : http://tokidoki-web.com/2013/02/%E3%80%8Ejquery%E3%81%A7%E7%89%B9%E5%AE%9A%E3%81%AEurl%E3%81%8C%E4%B8%80%E8%87%B4%E3%81%97%E3%81%9F%E3%82%89%E5%87%A6%E7%90%86%E3%81%97%E3%81%A6%E3%82%84%E3%82%93%E3%82%88%E3%80%8F/
-  if(document.URL.match("/users/edit")) {
+  if((document.URL.match("/users/edit")) || (document.URL.match("/groups/new"))) {
         $('.dropify').dropify();
     }
-    if(document.URL.match("/groups/new")) {
-        $('.dropify').dropify();
-    }
-
 });
--- app/assets/javascripts/modal_div.js
+++ app/assets/javascripts/modal_div.js
@@ -1,20 +1,22 @@
 $(function() {
 
-  $('a[rel*=leanModal]').leanModal({
-    top: 50,                     // モーダルウィンドウの縦位置を指定
-    overlay : 0.8,               // 背面の透明度
-    closeButton: ".modal_close"  // 閉じるボタンのCSS classを指定
-  });
+  if((!document.URL.match("/users/")) && (!document.URL.match("/groups/"))) {
+    $('a[rel*=leanModal]').leanModal({
+      top: 50,                     // モーダルウィンドウの縦位置を指定
+      overlay : 0.8,               // 背面の透明度
+      closeButton: ".modal_close"  // 閉じるボタンのCSS classを指定
+    });
 
-  $('.members-slide-button').on('click', function (){
-    // 押したpタグのvalueを取得
-    var user_group_no = $(this).attr("value");
-    // 対応するdivを表示 or 非表示
-    // http://kachibito.net/snippets/open-slide-panel
-    $('.modal-bottom-' + user_group_no).slideToggle(200);
-  });
+    $('.members-slide-button').on('click', function (){
+      // 押したpタグのvalueを取得
+      var user_group_no = $(this).attr("value");
+      // 対応するdivを表示 or 非表示
+      // http://kachibito.net/snippets/open-slide-panel
+      $('.modal-bottom-' + user_group_no).slideToggle(200);
+    });
 
-  // グループページで，現在のグループの背景・文字色を変える
-  $('.modal-user-group-div-' + gon.user_group_id).css('background-color', '#556068');
-  $('.modal-user-group-div-' + gon.user_group_id + " p").css('color', 'white');
+    // グループページで，現在のグループの背景・文字色を変える
+    $('.modal-user-group-div-' + gon.user_group_id).css('background-color', '#556068');
+    $('.modal-user-group-div-' + gon.user_group_id + " p").css('color', 'white');
+  }
 });
