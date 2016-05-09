cffa3a7ff3bc6129e8e64b84f3e9ccd6ffa1d0e2
片岡一平 <tonpurin2510@gmail.com>
2016-05-09 16:26:29 +0900

    得票数で順位付け owners#indexで整形している railsのgroup by使いにくい...

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,10 +2,30 @@ class OwnersController < ApplicationController
 
   def index
     # 対象のgroupの投票を集約
-    # 投票されたアイテムのユニーク
-    @voted_items = Group.find(UserGroup.group_id).user_items.group(:item_id).includes(:item)
-    # 投票の多い順にidと得票数をハッシュでソート
-    @vote_num = @voted_items.order('count_item_id DESC').count(:item_id)
+    # 投票されたアイテム...includesが出来ない
+    voted_items = Group.find(UserGroup.group_id).user_items
+    # 集計，得票数の多い順にソート，idと得票数のハッシュを取得
+    voted_counts = voted_items.group(:item_id).order('count_item_id DESC').count(:item_id).to_a # 配列変換
+
+    # 集計結果を使って並び替え...
+    @orderd_voted_items = convert_voted_item(voted_items, voted_counts)
+
+  end
+
+  private
+  def convert_voted_item(voted_items, voted_counts)
+    """
+    投票されたアイテムを得票数で並び替え,
+    カウント数とレコードを要素とした配列を作成する
+    """
+    converted_voted_item = []
+    voted_counts.each do |voted_count|
+      item_id = voted_count[0]
+      vote_count = voted_count[1]
+
+      converted_voted_item << [vote_count, voted_items.where(:item_id => item_id)[0]]
+    end
+    return converted_voted_item
   end
 
 end
