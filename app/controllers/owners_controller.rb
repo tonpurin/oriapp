bb577200147b8083f4d85b784c27daf6d976ac61
片岡一平 <tonpurin2510@gmail.com>
2016-05-09 15:40:37 +0900

    アクションでグループの投票結果を集約し，部分テンプレートで表示

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,7 +1,11 @@
 class OwnersController < ApplicationController
 
   def index
-    binding.pry
+    # 対象のgroupの投票を集約
+    # 投票されたアイテムのユニーク
+    @voted_items = Group.find(UserGroup.group_id).user_items.group(:item_id).includes(:item)
+    # 投票の多い順にidと得票数をハッシュでソート
+    @vote_num = @voted_items.order('count_item_id DESC').count(:item_id)
   end
 
 end
