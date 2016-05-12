9ecbdc026671aa98c9c64ff2f8c7e9c8166a7177
片岡一平 <tonpurin2510@gmail.com>
2016-05-12 15:31:18 +0900

    検索結果を表示する部分をトグルで表示・非表示

--- app/assets/javascripts/owners.js
+++ app/assets/javascripts/owners.js
@@ -3,5 +3,9 @@ $(function() {
   // google map初期化
   if(document.URL.match("/owners/")){
     owner_init_gmap();
+
+    $(".display-slide-button").on("click", function(){
+      $(".display-hotels").slideToggle(200);;
+    });
   }
 });
