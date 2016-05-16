c6b33d67f5060ed292ddbc4e4b858d160d6d5814
片岡一平 <tonpurin2510@gmail.com>
2016-05-17 01:15:11 +0900

    ownerのページのgoogle mapを修正 選択したらマーカーのデザインを変更 代理マーカーの表示

--- app/assets/javascripts/owners.js
+++ app/assets/javascripts/owners.js
@@ -33,23 +33,46 @@ $(function() {
 
       gon.current_item_id = item_id;
 
+      return [item_lat, item_lng];
+
     };
 
     toggle_iine_yokunaine_owner = function () {
-
-    // ユーザに選択されたアイテムか否かを判断してボタンをトグル
-    // $.inArray(要素, 配列)：要素が配列に存在すればそのインデックスを返す，なければ-1
-    if ($.inArray(gon.current_item_id, gon.group_items_info[0]) >= 0){
-      $('.iine-button').hide();
-      $('.yokunaine-button').show();
-    }else {
-      $('.iine-button').show();
-      $('.yokunaine-button').hide();
+      // ユーザに選択されたアイテムか否かを判断してボタンをトグル
+      // $.inArray(要素, 配列)：要素が配列に存在すればそのインデックスを返す，なければ-1
+      if ($.inArray(gon.current_item_id, gon.group_items_info[0]) >= 0){
+        $('.iine-button').hide();
+        $('.yokunaine-button').show();
+      }else {
+        $('.iine-button').show();
+        $('.yokunaine-button').hide();
+      };
     };
-  };
+
+    selected_item_click = function (){
+      // ユーザが選択したアイテムに対するイベント
+      // クリックした瞬間にボーダーを設定
+      // 対象のマーカーを目立たせる
+      $('.item-info-middle').click(
+        function (){
+          if (current_selected_dom != undefined) {
+            $(current_selected_dom).css("border", "");
+          };
+          var item_li = $(this).parent();
+          item_li.css("border", "solid 5px #5EABE6");
+          current_selected_dom = item_li;
+
+          // マーカーの変更
+          var marker_id = parseInt($(item_li).data('id'));
+          chenge_marker_design(marker_id);
+      });
+    };
 
     // google map初期化
     owner_init_gmap();
+
+    // ユーザが選択したアイテムに対するイベント
+    selected_item_click();
 
     $(".display-slide-button").on("click", function(){
       $(".display-hotels").slideToggle(200);;
